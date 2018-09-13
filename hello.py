c1865a1a3032b02a7b507739613dde318ae0c8b3
harbinzhang <seashore.real@gmail.com>
2018-09-12 17:56:00 -0700

    auto

--- hello.py
+++ hello.py
@@ -1,8 +1,6 @@
 from flask import Flask, request
 
 app = Flask(__name__)
-app.config['TESTING'] = True
-app.config['FLASK_ENV'] = "development"
 
 @app.route('/', methods=['GET'])
 def de_get():
@@ -12,4 +10,10 @@ def de_get():
 def do_post():
     if "challenge" in request.form:
         return request.form["challenge"]
+    for it in request.form:
+        print it
     return "hi"
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host ='0.0.0.0', port=5000)
